64ee2466d6038c05cfe6c3976c97c3642cc8d77b
Marian Meres <marian@meres.sk>
2025-07-02 11:10:52 +0200

    type

--- src/template-helpers/qsa.ts
+++ src/template-helpers/qsa.ts
@@ -5,6 +5,6 @@ declare const document: any;
 export function qsa(
 	selector: string,
 	context?: any /*HTMLElement | Document*/
-) {
+): any[] {
 	return Array.from((context ?? document)?.querySelectorAll(selector) || []);
 }
